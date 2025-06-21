be324241f57bdd83aec30dde74786c73ce49f5ef
majd alkaseeh <148889739+Majd-oreo@users.noreply.github.com>
2025-06-21 22:21:59 +0300

    Routes

--- ButterFly_effect_React/src/App.jsx
+++ ButterFly_effect_React/src/App.jsx
@@ -1,54 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 import FixedPlugin from './FixedPlugin';
-import DashboardCards from './DashboardCards';
-import SalesOverview from './SalesOverview';
-import Carousel from './Carousel';
-import SalesByCountry from './SalesByCountry';
-import Categories from './Categories';
+import DashboardAdmin from './DashBoardAdmin';
+import SignIn from './SignIn';
 
 function App() {
   return (
-    <div className="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
-      <div className="absolute w-full bg-blue-500 dark:hidden min-h-75"></div>
-      
-      <Sidebar />
-      
-      <main className="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
-        <Header />
-        
-        <div className="w-full px-6 py-6 mx-auto">
-          {/* Dashboard Cards Row */}
-          <DashboardCards />
-          
-          {/* Sales Overview and Carousel Row */}
-          <div className="flex flex-wrap mt-6 -mx-3">
-            <div className="w-full max-w-full px-3 mt-0 lg:w-7/12 lg:flex-none">
-              <SalesOverview />
-            </div>
-            <div className="w-full max-w-full px-3 lg:w-5/12 lg:flex-none">
-              <Carousel />
-            </div>
-          </div>
-          
-          {/* Sales by Country and Categories Row */}
-          <div className="flex flex-wrap mt-6 -mx-3">
-            <div className="w-full max-w-full px-3 mt-0 mb-6 lg:mb-0 lg:w-7/12 lg:flex-none">
-              <SalesByCountry />
-            </div>
-            <div className="w-full max-w-full px-3 mt-0 lg:w-5/12 lg:flex-none">
-              <Categories />
-            </div>
-          </div>
-          
-          <Footer />
-        </div>
-      </main>
-      
-      <FixedPlugin />
-    </div>
+    <Router>
+      <div className="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
+        <div className="absolute w-full bg-blue-500 dark:hidden min-h-75"></div>
+
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+
+          <Route
+            path="/admin"
+            element={
+              <>
+                <Sidebar />
+                <main className="relative h-full max-h-screen transition-all duration-200 ease-in-out xl:ml-68 rounded-xl">
+                  <Header />
+                  <DashboardAdmin />
+                  <Footer />
+                </main>
+                <FixedPlugin />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
